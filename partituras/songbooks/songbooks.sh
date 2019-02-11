c95ca1ab387fe4dbbf6892cae46793b0876996c5
rodrigoesborges <rodrigoesborges@gmail.com>
2019-02-11 12:28:34 -0200

    Script Songbook - otimização e limpeza de arquivos temporários

--- partituras/songbooks/songbooks.sh
+++ partituras/songbooks/songbooks.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
+#rascunho da criação da capa
+convert ../../img/fotos_2015/foto7.jpg -gravity Center -density 72 -pointsize 30 caption:"Vai Quem Fica\nSongbook Carnaval    $(date +%Y)\nv. $(date +%d-%m-%Y)\n\n" -append  -background white -size 595x842 -page a4 capa.pdf
+
+
+#Captura e Geração de nomes dos blocos em pdf
+
+#Captura
 grep -e 'C.pdf' -e '^  - nome' ../../repertorio.md | sed "s/.*: \+\\x27\(.*\)\x27/\1/g" - > listaC.txt 
 grep nome listaC.txt | sed 's/.* "\(.*\)"/\1/g'|  sed 's/ \([[:alpha:]]\)/_\1/g' - > blocos.txt
+
+#geração dos pdfs
 while read i 
 do 
-  convert -density 150 -size 595x842 -pointsize 50 caption:"\n\n   BLOCO\n\n   ${i}" -page A4 "$i".pdf
+  convert -page a4 -density 150 -pointsize 50 caption:"\nBLOCO\n\n${i}" -gravity Center -density 72 -size 595x842 "$i".pdf
 done < blocos.txt 
 
-sed 's/partituras/../g' listaC.txt | sed  's/.* "\(.*\)"/\1\.pdf/g' | sed 's/ \([[:alpha:]]\)/_\1/g' - > listaCcomblocos.txt
-pdftk capa.pdf $(cat listaCcomblocos.txt) cat output songbookC.pdf
+#livros a serem gerados
+tons_livros=(C Bb Eb Eb_notas letra)
+
+#rastreia músicas com partituras, coloca em lista na mesma ordem, gera o pdf de capa com demais elementos.
+for t in ${tons_livros[@]}; do
+        grep -e $t'.pdf' -e '^  - nome' ../../repertorio.md | sed "s/.*: \+\\x27\(.*\)\x27/\1/g" - > lista$t'.txt'
+	sed 's/partituras/../g' lista$t.txt | sed  's/.* "\(.*\)"/\1\.pdf/g' | sed 's/ \([[:alpha:]]\)/_\1/g' - > lista$t'comblocos.txt'
+	pdftk capa.pdf $(cat lista${t}'comblocos.txt') cat output songbook$t'.pdf'
+done
+
+
+#Limpa arquivos provisórios gerados
+shopt -s extglob
+rm !(song*).pdf
+rm *.txt
 
 
 
-grep -e 'Bb.pdf' -e '^  - nome' ../../repertorio.md | sed "s/.*: \+\\x27\(.*\)\x27/\1/g" - > listaBb.txt 
-sed 's/partituras/../g' listaBb.txt | sed  's/.* "\(.*\)"/\1\.pdf/g'| sed 's/ \([[:alpha:]]\)/_\1/g' - > listaBbcomblocos.txt
-pdftk capa.pdf $(cat listaBbcomblocos.txt) cat output songbookBb.pdf
-
-
-
-grep -e 'Eb.pdf' -e '^  - nome' ../../repertorio.md | sed "s/.*: \+\\x27\(.*\)\x27/\1/g" - > listaEb.txt 
-sed 's/partituras/../g' listaEb.txt | sed  's/.* "\(.*\)"/\1\.pdf/g'| sed 's/ \([[:alpha:]]\)/_\1/g' - > listaEbcomblocos.txt
-pdftk capa.pdf $(cat listaEbcomblocos.txt) cat output songbookEb.pdf
-
-grep -e 'Eb_notas.pdf' -e '^  - nome' ../../repertorio.md | sed "s/.*: \+\\x27\(.*\)\x27/\1/g" - > listaEbnotas.txt 
-sed 's/partituras/../g' listaEbnotas.txt | sed  's/.* "\(.*\)"/\1\.pdf/g'| sed 's/ \([[:alpha:]]\)/_\1/g' - > listaEbnotascomblocos.txt
-pdftk capa.pdf $(cat listaEbnotascomblocos.txt) cat output songbookEbnotas.pdf
-
-
-
-
-
-
-# Falta criar arquivos com nomes dos blocos a partir dos nomes, e incluir na lista de arquivos
-
-
-#for i in ; do
-# convert -density 150 -size 595x842 -pointsize 50 caption:"\n\n   BLOCOX" -page A4 $.pdf
-#rascunho da criação da capa
-#convert desenhocapa.png -density 150 -pointsize 30 caption:"\n\n                           Songbook\n                2019\n                          v. 02-02-2019" -append  -page A4 capa.pdf
