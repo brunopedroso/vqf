88466f1ad9bb33841789070636d27af44cc99c1c
rodrigoesborges <rodrigoesborges@gmail.com>
2020-01-28 13:51:30 -0300

    Ajustes para impressão correta e com detalhes dos songbooks

--- partituras/songbooks/songbooks.sh
+++ partituras/songbooks/songbooks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #rascunho da criação da capa
-convert ../../img/2020-capasongbook.png -gravity Center -density 72 -pointsize 30 caption:"Vai Quem Fica\nSongbook Carnaval    $(date +%Y)\nv. $(date +%d-%m-%Y)\n\n" -append  -background white -size 595x842 -page a4 capa.pdf
-
+convert ../../img/2020-capasongbook.png -gravity center -density 72 -pointsize 20 caption:"Vai Quem Fica\nSongbook Carnaval $(date +%Y)\nv.$(date +%d-%m-%Y) \n \n\n" -append  -background white -size 595x842 -page a4 capa.pdf
+#-size -page a4
 arqrep="../../_data/repertorio2020.yml"
 
 #Captura e Geração de nomes dos blocos em pdf
@@ -14,7 +14,7 @@ grep nome listaC.txt | sed 's/.* "\(.*\)"/\1/g'|  sed 's/ \([[:alpha:]]\)/_\1/g'
 #geração dos pdfs
 while read i 
 do 
-  convert -page a4 -density 150 -pointsize 50 caption:"\nBLOCO\n\n${i}" -gravity Center -density 72 -size 595x842 "$i".pdf
+  convert -page a4 -density 150 -pointsize 40 caption:"\nBLOCO\n\n${i}" -gravity center -density 72 -extent 595x842 "$i".pdf
 done < blocos.txt 
 
 #livros a serem gerados
@@ -24,7 +24,9 @@ tons_livros=(C Bb Eb Eb_notas letra)
 for t in ${tons_livros[@]}; do
         grep -e $t'.pdf' -e '^  - nome' $arqrep | sed "s/.*: \+\\x27\(.*\)\x27/\1/g" - > lista$t'.txt'
 	sed 's/partituras/../g' lista$t.txt | sed  's/.* "\(.*\)"/\1\.pdf/g' | sed 's/ \([[:alpha:]]\)/_\1/g' - > lista$t'comblocos.txt'
-	pdftk capa.pdf $(cat lista${t}'comblocos.txt') cat output songbook$t'.pdf'
+	convert -page a4 -density 150 -pointsize 12 caption:"${t}" -rotate -39 -gravity SouthWest -density 72 -extent 595x842 -transparent white "$t".pdf;
+	pdftk capa.pdf stamp $t.pdf  output capalivro.pdf
+	pdftk capalivro.pdf $(cat lista${t}'comblocos.txt') cat output songbook$t'.pdf'
 done
 
 
@@ -36,9 +38,11 @@ pdftk A=basei.pdf B=basep.pdf shuffle A B output songbookletrabooklet.pdf
 
 #| ps2pdf -sPAPERSIZE=a4  - songbookletrabooklet.pdf
 
+#criar estampa das capas com os tons
+
+
 #Limpa arquivos provisórios gerados
 shopt -s extglob
 rm !(song*).pdf
 rm *.txt
-
 
